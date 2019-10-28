032f6ed3d9f9fed82591aef725e56697fd1a0bad
Italo Perez <iep4@njit.edu>
2019-10-28 18:46:24 -0400

    psql set up

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SLQALCHEMY_DATABASE_URI']='postgresql://'
+db=SQLAlchemy(app)
+
+
+class Data(db.Model):
+    __tablename__= "data"
+    id=db.Column(db.Integer, primary_key=True)
+    email_= db.Column(db.String(120),unique=True)
+    height_=db.Column(db.Integer)
+
+    def __init__(self,email_,height_):
+        self.email_=email_
+        self.height_=height_
 
 
 @app.route("/")
@@ -13,9 +27,7 @@ def success():
     if request.method == 'POST':
         email = request.form['email_name']
         height = request.form['height_name']
-        print(request.form)
-        print(height)
-        print(email)
+        print(email,height)
         return render_template("success.html")
 
 
